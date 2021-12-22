ab54f7983135189b58819f0195d1194d2159e4a2
youHaveToDo <87919383+youHaveToDo@users.noreply.github.com>
2021-12-22 23:11:46 +0900

    add font-family

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyles from "./components/GlobalStyles";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyles />
+      <Test>123123132안녕하세요</Test>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+const Test = styled.div`
+  font-size: 20px;
+`;
