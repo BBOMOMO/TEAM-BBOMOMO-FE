4dfe41087695fed04e8bd874246257298e216beb
hamkke <walwalzz@naver.com>
2021-12-28 17:07:00 +0900

    GroupBx.css 병합

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import GlobalStyles from "./components/GlobalStyles";
 import { Route } from "react-router-dom";
 import "./styles/css/login.css";
 import "./styles/css/signup.css";
-import "./styles/css/groupbx.css";
+import "./styles/css/Group.css";
 import "./styles/css/grouprecommend.css";
 import "./styles/css/timer.css";
 import "./styles/css/myInfo.css";
@@ -15,7 +15,6 @@ import Signup from "./page/Singup";
 import PostChat from "./components/PostChat";
 import GroupRecommend from "./components/GroupRecommend";
 import GroupRoomTimer from "./components/GroupRoomTimer";
-import GroupBx from "./components/GroupBx";
 import Header from "./components/Header";
 import MyInfo from "./components/MyInfo";
 import UserView from "./components/UserView";
@@ -24,14 +23,13 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <Header/>
+      <Header />
       <Route path="/" exact component={Signup} />
       <Route path="/login" exact component={Login} />
       <Route path="/list" exact component={GroupList} />
       <Route path="/chat" exact component={PostChat} />
       <Route path="/groupreco" exact component={GroupRecommend} />
       <Route path="/timer" exact component={GroupRoomTimer} />
-      <Route path="/glist" exact component={GroupBx} />
       <Route path="/myinfo" exact component={MyInfo} />
       <Route path="/userview" exact component={UserView} />
     </>
