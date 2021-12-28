e050cfd3ca59dfdcb79e553f400d8f41826999f4
ireneeming <63698668+ireneeming@users.noreply.github.com>
2021-12-28 19:27:15 +0900

    main combined components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import GlobalStyles from "./components/GlobalStyles";
 import { Route } from "react-router-dom";
 import "./styles/css/login.css";
 import "./styles/css/signup.css";
-import "./styles/css/Group.css";
+import "./styles/css/group.css";
 import "./styles/css/grouprecommend.css";
 import "./styles/css/timer.css";
 import "./styles/css/myInfo.css";
@@ -15,7 +15,6 @@ import Signup from "./page/Singup";
 import PostChat from "./components/PostChat";
 import GroupRoomTimer from "./components/GroupRoomTimer";
 import Header from "./components/Header";
-import MyInfo from "./components/MyInfo";
 import UserView from "./components/UserView";
 import Main from "./page/Main";
 
@@ -29,7 +28,6 @@ function App() {
       <Route path="/list" exact component={GroupList} />
       <Route path="/chat" exact component={PostChat} />
       <Route path="/timer" exact component={GroupRoomTimer} />
-      <Route path="/myinfo" exact component={MyInfo} />
       <Route path="/userview" exact component={UserView} />
       <Route path="/main" exact component={Main} />
     </>
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,14 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import MyInfo from "../components/MyInfo";
 import GroupRecommend from "../components/GroupRecommend";
 
 const Main = (props) => {
   return (
     <>
-      <MyInfo />
-      <GroupRecommend />
+      <MainContainer>
+        <MyInfo />
+        <GroupRecommend />
+      </MainContainer>
+       
+     
+      
     </>
   );
 };
 
 export default Main;
+
+const MainContainer = styled.div`
+width:100%;
+height:auto; 
+display: flex;
+`;
