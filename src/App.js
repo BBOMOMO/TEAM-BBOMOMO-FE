581d52d8b9cc1ef04b8a32f352cdb21f8255b8dc
youHaveToDo <87919383+youHaveToDo@users.noreply.github.com>
2021-12-28 19:54:18 +0900

    make group page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import "./styles/css/timer.css";
 import "./styles/css/myInfo.css";
 import "./styles/css/userview.css";
 import Login from "./page/Login";
-import GroupList from "./page/GroupList";
 import Signup from "./page/Singup";
 import PostChat from "./components/PostChat";
 import GroupRoomTimer from "./components/GroupRoomTimer";
@@ -25,7 +24,6 @@ function App() {
       <Header />
       <Route path="/" exact component={Signup} />
       <Route path="/login" exact component={Login} />
-      <Route path="/list" exact component={GroupList} />
       <Route path="/chat" exact component={PostChat} />
       <Route path="/timer" exact component={GroupRoomTimer} />
       <Route path="/userview" exact component={UserView} />
---
+++ src/page/Group.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import UserView from "../components/UserView";
+
+const Group = () => {
+  return (
+    <>
+      <UserView />
+    </>
+  );
+};
+
+export default UserView;
