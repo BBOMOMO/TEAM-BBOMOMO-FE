cc3fd46ba8f1c6bef9cdcb8c9faa113a91cded1f
youHaveToDo <87919383+youHaveToDo@users.noreply.github.com>
2022-01-04 22:01:43 +0900

    fix: VideoChatRoom

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         <Route path="/writemodal" component={CertificationWrite} exact />
         <Route path="/commentmodal" component={CertificationComment} exact />
         <Route path="/certifi" component={Certification} exact />
-        <Route path="/video" exact component={VideoComponent} />
+        <Route path="/video/:roomId" exact component={VideoChatRoom} />
         <Route path={"*"} component={NotFound} />
       </Switch>
     </>
--- src/components/NoInfo.js
+++ src/components/NoInfo.js
@@ -3,31 +3,41 @@ import styled from "styled-components";
 import roundCircle from "../Images/timestat/notimestat.png";
 import user from "../Images/nouser.png";
 import pencil from "../Images/pencil.png";
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
-import {history} from "../redux/configureStore";
+import { history } from "../redux/configureStore";
 
 const NoInfo = (props) => {
-  
-
-
-
-
   return (
     <>
       <div className="myinfo_container">
         <div className="myinfo_profile_area">
-          <img src={roundCircle} style={{width:"260px"}}/>
+          <img src={roundCircle} style={{ width: "260px" }} />
           <img src={user} className="myinfo_user_img" />
         </div>
         <div className="myinfo_txt_area">
           <div className="myinfo_nouser_info">
             <h3 className="myinfo_user_name">
-              <span onClick={()=>{history.push("/login")}}> 로그인 </span> / 
-              <span onClick={()=>{history.push("/signup")}}> 회원가입 </span>
-              <ChevronRightIcon/></h3>
+              <span
+                onClick={() => {
+                  history.push("/login");
+                }}
+              >
+                {" "}
+                로그인{" "}
+              </span>{" "}
+              /
+              <span
+                onClick={() => {
+                  history.push("/signup");
+                }}
+              >
+                {" "}
+                회원가입{" "}
+              </span>
+              <ChevronRightIcon />
+            </h3>
           </div>
-         
         </div>
         <div className="myinfo_studytime">
           <div className="myinfo_studytime_top">
@@ -46,7 +56,6 @@ const NoInfo = (props) => {
         </div>
         <div className="myinfo_make_group disabled">
           <p>+ 그룹 만들기</p>
-         
         </div>
       </div>
     </>
@@ -54,4 +63,3 @@ const NoInfo = (props) => {
 };
 
 export default NoInfo;
-
