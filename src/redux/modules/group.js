58105c4268605b925f6e741c305c9d152789ca59
hamkke <walwalzz@naver.com>
2022-01-04 14:26:40 +0900

    SH

--- src/redux/modules/group.js
+++ src/redux/modules/group.js
@@ -4,73 +4,71 @@ import { setToken } from "../../shared/token";
 
 import apis from "../../shared/apis";
 
-
 // actions
 const LOAD_ROOMS = "LOAD_ROOMS";
 const ADD_ROOMS = "ADD_ROOMS";
 
-
 // action creators
-const loadRooms = createAction(LOAD_ROOMS, (room_list)=>({room_list}));
-const addRooms = createAction(ADD_ROOMS,(newRoom)=>({newRoom}));
-
+const loadRooms = createAction(LOAD_ROOMS, (room_list) => ({ room_list }));
+const addRooms = createAction(ADD_ROOMS, (newRoom) => ({ newRoom }));
 
 // initialState
 const initialState = {
-  roomList : [],
+  roomList: [],
 };
-
 
 // middlewares
 const getRooms = () => {
-  return async function(dispatch, useState, {history}){
-    await apis
-    .getRoom()
-    .then(function(response){
+  return async function (dispatch, useState, { history }) {
+    await apis.getRoom().then(function (response) {
       //console.log("getroom",response.data);
       dispatch(loadRooms(response));
-    })
+    });
+  };
+};
 
-  }
-}
-
-const addRoom = (userId, roomTitle,roomStatus, roomPassword, roomPurpose, round, studyTime, recessTime, openedAt) => {
-  return async function(dispatch, useState, {history}){
-    
+const addRoom = (
+  userId,
+  roomTitle,
+  roomStatus,
+  roomPassword,
+  roomPurpose,
+  round,
+  studyTime,
+  recessTime,
+  openedAt
+) => {
+  return async function (dispatch, useState, { history }) {
     const roomInfo = {
       roomTittle: roomTitle,
-      private : roomStatus,
+      private: roomStatus,
       roomPassword: roomPassword,
       purpose: roomPurpose,
       round: round,
       studyTime: studyTime,
       recessTime: recessTime,
-      openAt: openedAt
+      openAt: openedAt,
     };
-  
+
     await apis
-    .postRoom(userId,roomInfo )
-    .then(function(response){
-      console.log("방 생성 완료");
-      
-    }).catch((err)=>{
-      console.log(err.response.data.msg);
-    })
-
-  }
-}
-
-
+      .postRoom(userId, roomInfo)
+      .then(function (response) {
+        console.log("방 생성 완료");
+      })
+      .catch((err) => {
+        console.log(err.response.data.msg);
+      });
+  };
+};
 
 // reducer
 export default handleActions(
   {
-    [LOAD_ROOMS]:(state, action) => produce(state, (draft)=>{
-      draft.roomList = action.payload.room_list.data; 
-    }),
-    [ADD_ROOMS]:(state, action) => produce(state, (draft)=>{
-      
-    }),
+    [LOAD_ROOMS]: (state, action) =>
+      produce(state, (draft) => {
+        draft.roomList = action.payload.room_list.data;
+      }),
+    [ADD_ROOMS]: (state, action) => produce(state, (draft) => {}),
   },
   initialState
 );
@@ -79,5 +77,4 @@ export const actionCreators = {
   getRooms,
   loadRooms,
   addRoom,
-  
 };
