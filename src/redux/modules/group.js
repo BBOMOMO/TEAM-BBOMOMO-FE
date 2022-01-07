33466b710daf1a95a579667d056bcffbbcd529fa
youHaveToDo <87919383+youHaveToDo@users.noreply.github.com>
2022-01-08 01:42:32 +0900

    fix: enterRoom

--- src/redux/modules/group.js
+++ src/redux/modules/group.js
@@ -74,8 +74,8 @@ const enterRoom = (newRoomId, roomPassword = null) => {
       })
       .catch((err) => {
         console.log(err.response.data.msg);
-        // window.alert("공부시간에는 방에 입장할 수 없습니다!");
-        // history.push("/");
+        window.alert("공부시간에는 방에 입장할 수 없습니다!");
+        history.push("/");
       });
   };
 };
