f7eede2c8e3568977c73ce4818eebbbbc1feecd9
youHaveToDo <87919383+youHaveToDo@users.noreply.github.com>
2022-01-13 11:08:33 +0900

    add: lank

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -1,14 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
 
-function Rank({today, total}) {
+function Rank({ today, total }) {
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(userActions.getLankDB());
+  }, []);
+  const rankInfo = useSelector((state) => state);
+  console.log(rankInfo);
+
   return (
     <RankContainer>
       <div className="myinfo_studytime_mid">
         <p className="myinfo_studytime_today">랭크</p>
         <p className="myinfo_studytime_today_time">{today}</p>
       </div>
-      
+
       <div className="myinfo_studytime_bot">
         <p className="myinfo_studytime_total">랭크</p>
         <p className="myinfo_studytime_total_time">{total}</p>
@@ -25,14 +34,12 @@ function Rank({today, total}) {
         <p className="myinfo_studytime_total">랭크</p>
         <p className="myinfo_studytime_total_time">{total}</p>
       </div>
-      
     </RankContainer>
-  )
+  );
 }
 
 export default Rank;
 const RankContainer = styled.div`
-width:100%;
-height:224px;
-
+  width: 100%;
+  height: 224px;
 `;
