4f724602308a77f9efab3eedd556688e0726f6dd
hamkke <walwalzz@naver.com>
2022-01-20 01:18:51 +0900

    메인배너 작업 중

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/configureStore";
-import {ConnectedRouter} from "connected-react-router";
-import {history} from "./redux/configureStore";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "./redux/configureStore";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
